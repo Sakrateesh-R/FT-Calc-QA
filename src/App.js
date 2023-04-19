0072b100e4456575f80d6455c5695700ce55964b
Sakrateesh-R <sakrateesh007@gmail.com>
2023-04-19 07:53:16 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import Curreny from './components/Currency';
+import Currency from "./components/Currency"
 import Home from "./components/Home.js";
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
@@ -12,6 +12,8 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/currency' element={<Currency/>} />
+        <Route path ='/profit/loss' element={<ProfitLoss/>} />
       </Routes>
     </Router>
   );
