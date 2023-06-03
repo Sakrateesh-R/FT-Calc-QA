bd8e08b0779f7fd1ff538b215f4959b8ab6835ae
Sakrateesh-R <sakrateesh007@gmail.com>
2023-06-03 12:58:32 +0530

    Saka's first commit from victus
    Updated the Depreciation module

--- src/components/calculations/Depreciation.js
+++ src/components/calculations/Depreciation.js
@@ -22,38 +22,31 @@ export default function Deprecation(){
         calculateRender:false
     })
     console.log(depValue)
-    const [deprecationAmount,setDeprecationAmount] = useState({});
-    const [piedata, setPieData] = useState()
+    //const [deprecationAmount,setDeprecationAmount] = useState({});
+    const [piedata, setPieData] = useState({
+        OriginalAmount:0,
+        DeprecationAmount:0
+    })
 
-    var obj = new Object();
+    //var obj = new Object();
 
     function Calculate(){
         const loopnum = depValue.LOA;
         var Initial_Amount = depValue.CTA;
-
-        for(let dObject=0; dObject<=loopnum; dObject++){
-            console.log("in loop");
-        
-            console.log("in condition")
-            let depAmount = Initial_Amount - 100;
-            var swapAmount = depAmount;
-            obj = {[dObject]:swapAmount};
-            // console.log(obj);
-            Initial_Amount = swapAmount;
-            setDeprecationAmount({...deprecationAmount,dObject:Initial_Amount});
-            setCalculateRender({
-                calculateRender: true
-            })
-            setPieData({
-                OriginalAmount:Initial_Amount,
-                DeprecationAmount:deprecationAmount.dObject
-            })
-            console.log("Pie Date" +" "+ piedata)
-            setData({labels: ['Asset Rate at buying','Deprecation Value'],
+        var deprecation =  Initial_Amount * Math.pow((1 - depValue.SRValue / 100), depValue.LOA)
+        console.log(deprecation)
+        setCalculateRender({
+            calculateRender: true
+        })
+        setPieData({
+            OriginalAmount:Initial_Amount,
+            DeprecationAmount:deprecation
+        })
+        const chartData = {labels: ['Asset Rate at buying','Deprecation Value'],
             datasets: [
             {
                 label: "FT Calc",
-                data: [Initial_Amount,swapAmount],
+                data: [Initial_Amount,deprecation],
                 backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -64,87 +57,127 @@ export default function Deprecation(){
                 ],
                 borderWidth: 1,
             }
-            ],})
-            console.log(Initial_Amount)
+            ],}
+            setData(chartData)
+
+        // for(let dObject=0; dObject<=loopnum; dObject++){
+        //     console.log("in loop");
+        
+        //     console.log("in condition")
+        //     let depAmount = Initial_Amount - 100;
+        //     var swapAmount = depAmount;
+        //     obj = {[dObject]:swapAmount};
+        //     // console.log(obj);
+        //     Initial_Amount = swapAmount;
+        //     setDeprecationAmount({...deprecationAmount,dObject:Initial_Amount});
+        //     setCalculateRender({
+        //         calculateRender: true
+        //     })
+        //     setPieData({
+        //         OriginalAmount:Initial_Amount,
+        //         DeprecationAmount:deprecationAmount.dObject
+        //     })
+        //     console.log("Pie Date" +" "+ piedata)
+        //     setData({labels: ['Asset Rate at buying','Deprecation Value'],
+        //     datasets: [
+        //     {
+        //         label: "FT Calc",
+        //         data: [Initial_Amount,swapAmount],
+        //         backgroundColor: [
+        //         'rgba(255, 99, 132, 0.2)',
+        //         'rgba(54, 162, 235, 0.2)',
+        //         ],
+        //         borderColor: [
+        //         'rgba(255, 99, 132, 1)',
+        //         'rgba(54, 162, 235, 1)'
+        //         ],
+        //         borderWidth: 1,
+        //     }
+        //     ],})
+        //     console.log(Initial_Amount)
             
-        }
-        console.log(deprecationAmount)
-        console.log(obj);
+        // }
+        console.log(piedata.DeprecationAmount)
+        // console.log(obj);
     }
     
-    function Reset(){
-        setDepValue({});
-        setCalculateRender({
-            calculateRender:false
-        })
+    function Reset() {
+        if(depValue.CTA){
+            setDepValue({})
+            
+        }
+        setCalculateRender({calculateRender:false})
+        //setDisplayPie({value:false})
+       
     }
 
     return(
-        <div className="deprecation-container body-position-container">
-            <h1 style={{textAlign:'center'}}>Calculate Deprecation</h1>
-            <div className="Depreciation-container-body container">
-                <div className="Depreciation-calculate-section">
-                    <div className="Depreciation-input-section">
-                        <label className="form-label">Cost of the Asset</label><br/>
-                        <input type="number" className="form-control" name="CTA" onChange={handleChange}/><br/>
-                        <label className="form-label">Salvage/Residual value (in percentage %)</label><br/>
-                        <input type="number" className="form-control" name="SRValue" onChange={handleChange}/><br/>
-                        {/* <label className="form-label">Method of Depreciation</label><br/>
-                        <input type="number" className="form-control"/><br/> */}
-                        <label className="form-label">Life of Asset</label><br/>
-                        <input type="range" className="form-range" name="LOA" data-bs-toggle="tooltip" data-bs-placement="top" title={`${depValue.LOA} years`} onChange={handleChange} min={0} max={100}/>
-                    </div>
-                </div>
-                <button  onClick={Calculate} className="btn-calc btn btn-primary"> FT Clac Interest </button>
-                <button className="btn btn-danger" onClick={Reset}> Reset </button>
-            </div>
-            {
-                deprecationAmount.dObject? 
-                <div>
-                    <div className="Interest-Result-Section-right" >
-                        <h2>Your Deprecation Value</h2>
-                        <div className="Interest-Result-Overview">
-                            <h4 className="form-label" >Asset Deprecation Amount : </h4>
-                            <h4>₹ {!deprecationAmount.dObject? 0 : deprecationAmount.dObject}</h4>
-                            <h4 className="form-label" >Asset Original Amount : </h4>
-                            <h4 >₹ {!depValue.CTA ? 0 : depValue.CTA}</h4>
+        <div className="body-position-container">
+            <div className="Home-page-container col-lg-12">
+                <div className="Home-Interest-Section-Container">
+                    <div className="Interest-Section-container">
+                    
+                        <div className="Interest-container">
+                            
+                            <div className="Interest-container-section1">
+                                <h1 id="CI" className="h1">Calculate Interest</h1>
+                                <label className="form-label"> Assest Original Value</label><br/>
+                                <input type="number" pattern="[0-9]" className="form-control" id = "totalAmount" name = "CTA" value={depValue.CTA} onFocus={ (e) => e.target.value=''} onChange={handleChange} onBlur ={(e) => e.target.value = depValue.CTA} /> <br/>
+                                <label className="form-label">Deprecation Percentage Per Year</label><br/>
+                                <input type="number" className="form-control" id = "rateOfInterest" name = "LOA" value={depValue.LOA} onFocus={ (e) => e.target.value=''} onBlur ={(e) => e.target.value = depValue.LOA} onChange={handleChange} /> <br/>
+                                <label className="form-label">Assest Useful Years</label><br/>
+                                <input type="number" className="form-control" id = "noOfYear" name = "SRValue" value={depValue.SRValue} onFocus={ (e) => e.target.value=''} onBlur ={(e) => e.target.value = depValue.SRValue} onChange={handleChange} /> <br/><br/>
+                                <button  onClick={Calculate} className="btn-calc btn btn-primary"> FT Clac Interest </button>
+                                <button className="btn btn-danger" onClick={Reset}> Reset </button>
+                                
+                            </div>
+                            <br/>
+                            
+                            
+                        </div> 
+                        { !calculateRender.calculateRender? console.log("not rendered") :
+                        <div className="Dynamic-interest-section">
+                            <div className="Interest-Result">
+                                <h1>Your Loan Estimate</h1>
+                                <div className="Interest-Result-Section">
+                                    <div className="Interest-Result-Section-left" >
+                                        <h2>Assest Value after Depreciation </h2>
+                                        <h4>₹{piedata.DeprecationAmount ? 0 :piedata.DeprecationAmount}</h4> 
+                                    </div>
+                                    <div className="Interest-Result-Section-right" >
+                                        <h2>Overview</h2>
+                                        <div className="Interest-Result-Overview">
+                                            <h4 className="form-label" >Asset Buying Price </h4>
+                                            <h4>₹ {!depValue.CTA? 0 : depValue.CTA }</h4>
+                                            <h4 className="form-label" >Depreciation rate per year</h4>
+                                            <h4 >₹ {!depValue.LOA ? 0 : depValue.LOA}</h4>
+                                            <h4 className="form-label" >Deprecation rate per year  </h4>
+                                            <h4>₹ {!depValue.SRValue? 0 : depValue.SRValue}</h4>
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                                </div> <br/>
+                                
+                                {/* <div className="Interest-Pie-chart">
+                                    <h1>Pie data section</h1>
+                                    <Pie 
+                                    data={data} 
+                                    updateMode = "reset"
+                                    />
+                                </div> */}
+                            
+                            </div>
+                        }
                         </div>
-                        {/* <div className="Interest-Pie-chart">
-                            <h1>Pie data section</h1>
-                            <Pie 
-                            data={data} 
-                            updateMode = "reset"
-                            />
-                        </div> */}
-                    </div>
-                    <div className="Interest-Pie-chart">
-                    <h1>Pie data section</h1>
-                    <Pie 
-                    data={data} 
-                    updateMode = "reset"
-                    />
-                    </div>
-                </div>
-                // <div className="Depreciation-calculation">
-                //     <div className="Depreciation-section-value">
-                //         <h2></h2>
-                //         <div className="Depreciation-calculate-section">
-                //             <h3> {}</h3>
-                //             <h3> {depValue.CTA}</h3>
-                //         </div>
-                //         <div className="Interest-Pie-chart">
-                //             <h1>Pie data section</h1>
-                //             <Pie 
-                //             data={data} 
-                //             updateMode = "reset"
-                //             />
-                //         </div>
-                //     </div>
-                // </div> 
-                :
-                console.log("not calculated")
-            }
-            
-        </div>
+                        
+                        <br/>
+                    <label>
+                        <b>Note:</b> Perform one calculation at a time and reset the calculation and proceed with next one.
+                    </label>
+                </div>           
+                    
+            </div> 
+        </div>      
     )
 }
